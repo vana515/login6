f2a0cfe3fd878ac68fee3b78afe5575f497f4562
vana515 <jovanaascenciogarcia@gmail.com>
2024-11-10 21:50:34 -0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login6</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/cabecera.css">
+</head>
+<body>
+  <form action="validar.php" metgod="post">
+    <h1>LOGIN</h1>
+    <p>Usuario <input type="text" placeholder="ingrese su nombre" name="usuario"></p>
+    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
+    <input type="submit" value="ingresar">
+</body>
+</html>
